60bf795c545a42dc1620287fc56df662cb68e3b0
Eleven <jun.xi@ximalaya.com>
2021-09-01 15:34:09 +0800

    perf: update gitalk comments order

--- docs/app.js
+++ docs/app.js
@@ -1,18 +1,9 @@
 'use strict';
 
-var gitalkConfig = {
-  clientID: '2f7463c94730f4b6aebc',
-  clientSecret: 'c183974bbbaad88fbc07acf2868f9be06d652975',
-  repo: 'webpack.eleven.net.cn',
-  owner: 'eleven-net-cn',
-  admin: ['eleven-net-cn'],
-  distractionFreeMode: false,
-  proxy: 'https://cors-anywhere.azm.workers.dev/https://github.com/login/oauth/access_token',
-};
-
 window.$docsify = {
   el: '#app',
   name: 'webpack.eleven.net.cn',
+  nameLink: '/',
   repo: 'true',
   // routerMode: 'history',
   corner: {
@@ -83,6 +74,17 @@ window.$docsify = {
       });
     },
   ],
+};
+
+var gitalkConfig = {
+  clientID: '2f7463c94730f4b6aebc',
+  clientSecret: 'c183974bbbaad88fbc07acf2868f9be06d652975',
+  repo: 'webpack.eleven.net.cn',
+  owner: 'eleven-net-cn',
+  admin: ['eleven-net-cn'],
+  distractionFreeMode: false,
+  proxy: 'https://cors-anywhere.azm.workers.dev/https://github.com/login/oauth/access_token',
+  pagerDirection: 'first',
 };
 
 function initGitalk(vm) {
